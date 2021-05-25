58ee4944ac069ca5f7801892d708095dd887f47b
chepulenis <chepulenis@gmail.com>
2021-05-25 19:45:45 +0300

    empty project

---
+++ mywidgetextension/backoffice/src/training/my/services/MywidgetextensionService.java
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2020 SAP SE or an SAP affiliate company. All rights reserved
+ */
+package training.my.services;
+
+/**
+ * Hello World MywidgetextensionService
+ */
+public class MywidgetextensionService
+{
+	public String getHello()
+	{
+		return "Hello";
+	}
+}
